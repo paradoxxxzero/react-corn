fd43721987cccb2dcef11417e40b92b3388a55e3
Florian Mounier <florian.mounier@kozea.fr>
2020-06-11 16:34:49 +0200

    FIx material-ui-quill build config

--- packages/material-ui-quill/rollup.config.js
+++ packages/material-ui-quill/rollup.config.js
@@ -8,6 +8,10 @@ export default {
     '@react-corn/material-ui',
     '@react-corn/quill',
     '@material-ui/core',
+    '@material-ui/core/Input/Input',
+    '@material-ui/core/FilledInput/FilledInput',
+    '@material-ui/core/OutlinedInput/OutlinedInput',
+    '@material-ui/core/OutlinedInput/NotchedOutline',
     '@material-ui/core/styles',
   ],
 }
