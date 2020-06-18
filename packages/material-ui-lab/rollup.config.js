18541f95fef038217d80ec01dea59d785fb8747a
Florian Mounier <florian.mounier@kozea.fr>
2020-06-18 16:39:31 +0200

    Add externals to build

--- packages/material-ui-lab/rollup.config.js
+++ packages/material-ui-lab/rollup.config.js
@@ -5,8 +5,12 @@ export default {
     ...rollupConfig.external,
     '@material-ui/core',
     '@material-ui/core/styles',
+    '@material-ui/utils',
     '@material-ui/pickers',
     'clsx',
+    'prop-types',
+    'hoist-non-react-statics',
+    'react-is',
     '@react-corn/core',
   ],
 }
